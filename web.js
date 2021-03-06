9bd98688c879d2d6a76bc232052a80d2458bfc70
uwasejeannine <jeannineuwasee@gmail.com>
2021-03-06 19:37:04 +0300

    assignment1

---
+++ web.js
@@ -0,0 +1,10 @@
+function fruits() {
+    var fruit = ["apples", "pineapples", "bananas","avocados"];
+    fruit.forEach((fruit, index) => {
+        console.log(index+ ":" + fruit)
+
+    });
+
+
+}
+fruits()
